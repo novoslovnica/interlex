51fe6ac0b03a0f32d35b1912be91a34b3f8a4f89
Карпов Егор Андреевич <karpov@myservices.digital>
2026-07-06 13:18:31 +0300

    rm .next

--- rebuild-remote.sh
+++ rebuild-remote.sh
@@ -2,6 +2,7 @@ cd /var/www/interslavic-lexicon.com/interlex
 git checkout main
 git pull
 rm package-lock.json
+rm -rf .next
 npm i
 npm run build
 sudo systemctl restart interslavic-lexicon.service
