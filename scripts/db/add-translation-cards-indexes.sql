a3180f4f0950488f3687fbf6d483a43538921fbb
Карпов Егор Андреевич <karpov@myservices.digital>
2026-07-16 12:51:50 +0300

    update indexes

---
+++ scripts/db/add-translation-cards-indexes.sql
@@ -0,0 +1,20 @@
+CREATE INDEX IF NOT EXISTS idx_meanings_lexemeId ON meanings(lexemeId);
+
+CREATE INDEX IF NOT EXISTS idx_en_meaningId_veryfied ON en(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_ru_meaningId_veryfied ON ru(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_mk_meaningId_veryfied ON mk(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_sr_meaningId_veryfied ON sr(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_uk_meaningId_veryfied ON uk(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_bg_meaningId_veryfied ON bg(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_pl_meaningId_veryfied ON pl(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_be_meaningId_veryfied ON be(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_cs_meaningId_veryfied ON cs(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_sk_meaningId_veryfied ON sk(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_sl_meaningId_veryfied ON sl(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_hr_meaningId_veryfied ON hr(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_hsb_meaningId_veryfied ON hsb(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_dsb_meaningId_veryfied ON dsb(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_cu_meaningId_veryfied ON cu(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_de_meaningId_veryfied ON de(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_nl_meaningId_veryfied ON nl(meaningId, veryfied);
+CREATE INDEX IF NOT EXISTS idx_eo_meaningId_veryfied ON eo(meaningId, veryfied);
